0842ffc6a525817fdad54e531c61b1a95bbcbdfa
Tim Snow <tim.snow@diamond.ac.uk>
2016-10-19 09:53:29 +0100

    Culmination of new changes from yesterday (forgot to commit before leaving) and some new this morning.

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/HermanOrientationModel.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/HermanOrientationModel.java
@@ -6,40 +6,71 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
+
 package uk.ac.diamond.scisoft.analysis.processing.operations;
 
 import org.eclipse.dawnsci.analysis.api.processing.model.OperationModelField;
+import uk.ac.diamond.scisoft.analysis.processing.operations.IntegrationModel;
 
-import uk.ac.diamond.scisoft.analysis.processing.operations.externaldata.ExternalDataModel;
+// Might not need these later on, let's see...
+// import org.eclipse.dawnsci.analysis.api.processing.model.FileType;
+// import uk.ac.diamond.scisoft.analysis.processing.operations.HermanOrientationModel.NumberOfPis;
 
-public class HermanOrientationModel extends ExternalDataModel {
 
-//	@OperationModelField Things for the UI
+// The model for a DAWN process to perform a Herman Orientation calculation on a given image
+public class HermanOrientationModel extends IntegrationModel {
 
-	@OperationModelField(label = "Dataset Name", hint = "The name of the dataset or the node path of the data if a .NXS file")
-	private String dataName = "";
+
+	// Let's give the user a fixed choice so they don't go too nuts...
+	enum NumberOfPis {
+		HALF_PI(1),
+		WHOLE_PI(2);
+		
+		private final int pis;
+		
+		NumberOfPis(int pis) {
+			this.pis = pis;
+		}
+		
+		public int getNumberOfPis() {
+			return pis;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("%d", this.pis);
+		}
+	}
+	
+	
+	//@OperationModelField annotations for the UI setup
+	// First the start angle of the integration N.B. 0 = North, going clockwise.
 	@OperationModelField(label = "Start Angle", hint = "A value between zero and 180 degrees, where zero is north and increasing angle is clockwise")
-	private double integrationStartAngle = 25;
-	@OperationModelField(label = "Integration Range", hint = "Integrate over half the ring (Pi) or the whole ring (Two Pi)")
-	private double integrationRange = 25;
+	private double integrationStartAngle = 0.00;
 
+	// Now the getters and setters
 	public double getIntegrationStartAngle() {
 		return integrationStartAngle;
 	}
+
 	public void setIntegrationStartAngle(double integrationStartAngle) {
 		firePropertyChange("IntegrationStartAngle", this.integrationStartAngle, this.integrationStartAngle = integrationStartAngle);
 	}
+
+
+	// Should we be integrating over a half or one Pi radians?
+	@OperationModelField(label = "Integration Range", hint = "Integrate over half the ring (Pi) or the whole ring (Two Pi)")
+	private NumberOfPis integrationRange = NumberOfPis.WHOLE_PI;
+
+	// Now the getters and setters
 	public double getIntegrationRange() {
-		return integrationRange;
+		double piMultiplier = integrationRange.getNumberOfPis() / 2
+		return piMultiplier * Math.PI;
 	}
-	public void setIntegrationRange(double integrationRange) {
+
+
+	public void setIntegrationRange(NumberOfPis integrationRange) {
 		firePropertyChange("IntegrationRange", this.integrationRange, this.integrationRange = integrationRange);
-	}
-	public String getDataName() {
-		return dataName;
-	}
-	public void setDataname(String dataName) {
-		firePropertyChange("DataName", this.dataName, this.dataName = dataName);
 	}
 
 }
--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/HermanOrientationOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/HermanOrientationOperation.java
@@ -6,43 +6,98 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
+
 package uk.ac.diamond.scisoft.analysis.processing.operations;
 
+// Imports from java
+import java.util.List;
+import java.util.Arrays;
+
+// Imports from org.eclipse
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
 import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
-//import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
 import org.eclipse.january.IMonitor;
 import org.eclipse.january.dataset.IDataset;
+import org.eclipse.january.metadata.MaskMetadata;
 
-//import uk.ac.diamond.scisoft.analysis.io.LoaderFactory;
-//import uk.ac.diamond.scisoft.analysis.processing.operations.utils.OperationServiceLoader;
+// Imports from uk.ac.diamond
+import uk.ac.diamond.scisoft.analysis.roi.ROIProfile;
+import uk.ac.diamond.scisoft.ncd.processing.NcdOperationUtils;
+import uk.ac.diamond.scisoft.analysis.processing.io.NexusNcdMetadataReader;
 
+// Might not need this, we shall see...
+//import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
+
+
+// The operation for a DAWN process to perform a Herman Orientation calculation on a given image
 public class HermanOrientationOperation extends AbstractOperation<HermanOrientationModel, OperationData> {
 
 
+	// Let's give this process an ID tag
 	@Override
 	public String getId() {
 		return "uk.ac.diamond.scisoft.analysis.processing.operations.HermanOrientationOperation";
 	}
 
-	@Override
-	public OperationData process(IDataset dataset, IMonitor monitor) {
-		// get series metadata (will not be null), to check we are from the same
-		// parent and whether we have hit the final image
-		//SliceFromSeriesMetadata ssm = getSliceSeriesMetadata(dataset);
-		OperationData toReturn = new OperationData();
-		return toReturn;	
-	}
 
+	// Before we start, let's make sure we know how many dimensions of data are going in...
 	@Override
 	public OperationRank getInputRank() {
 		return OperationRank.TWO;
 	}
 
+
+	// ...and out
 	@Override
 	public OperationRank getOutputRank() {
 		return OperationRank.TWO;
 	}
+
+
+	// Now let's define the 'meat' of the process
+	@Override
+	public OperationData process(IDataset dataset, IMonitor monitor) throws OperationException {
+
+	// We will need to change the integration range as given here into an ROI at some point...
+		int hermanPiROI = model.getIntegrationRange();
+		// First get the region that we're interested in examining
+		IROI regionOfInterest = 1; // This is where the magic will need to come in.
+		// e.g. IROI roi = model.getRegion();
+
+		// Let's just work with the region of interest now
+		SectorROI sectorRoi = (SectorROI) hermanPiROI;
+
+		// Extract out some information about our data from dataset
+		int[] frames = NcdOperationUtils.addDimension(dataset.getShape());
+		int dimension = 2; // Matching our input rank
+		int[] areaShape = (int[]) ConvertUtils.convert(Arrays.copyOfRange(frames, frames.length - dimension, frames.length), int[].class);
+
+		// Create a home for the mask metadata and try to fetch it, if not possible raise an exception
+		List<MaskMetadata> mask;
+		try {
+			mask = dataset.getMetadata(MaskMetadata.class);
+		} catch (Exception e) {
+			throw new OperationException(this, e);
+		}
+
+		// Create a null mask and if possible, fill it with a mask from our dataset
+		Dataset maskDataset = null;
+		if (mask != null) {
+			maskDataset = DatasetUtils.convertToDataset(mask.get(0).getMask()).getSlice();
+		}
+
+
+
+
+		IDataset userData = dataset;
+
+
+
+
+		OperationData toReturn = new OperationData();
+		return toReturn;	
+	}
+
 
 }
