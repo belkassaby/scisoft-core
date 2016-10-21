eb7b569e78deb83dad94c91edb0dac981e1e7705
Tim Snow <tim.snow@diamond.ac.uk>
2016-10-21 15:21:38 +0100

    Just before large deletions

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/image/FourierTransformImageOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/image/FourierTransformImageOperation.java
@@ -1,27 +1,15 @@
 package uk.ac.diamond.scisoft.analysis.processing.operations.image;
 
-import org.eclipse.dawnsci.analysis.api.image.IImageTransform;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
-import org.eclipse.january.DatasetException;
 import org.eclipse.january.IMonitor;
-import org.eclipse.january.MetadataException;
 import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.FFT;
+import org.eclipse.january.dataset.FloatDataset;
 import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.january.dataset.DatasetUtils;
-import org.eclipse.january.dataset.IDataset;
-import org.eclipse.january.dataset.ILazyDataset;
-import org.eclipse.january.metadata.AxesMetadata;
-import org.eclipse.january.metadata.MetadataFactory;
-import org.eclipse.january.IMonitor;
-import org.eclipse.january.dataset.Dataset;
-import org.eclipse.january.dataset.DatasetFactory;
-import org.eclipse.january.dataset.IDataset;
-import org.eclipse.january.dataset.Slice;
-import org.eclipse.january.dataset.SliceND;
-import uk.ac.diamond.scisoft.analysis.processing.operations.utils.OperationServiceLoader;
-import org.eclipse.dawnsci.analysis.dataset.slicer.SliceFromSeriesMetadata;
 
 public class FourierTransformImageOperation extends AbstractSimpleImageOperation<FourierTransformImageModel> {
 
@@ -53,6 +41,7 @@ public class FourierTransformImageOperation extends AbstractSimpleImageOperation
 
 		// So that DAWN doesn't crash whilst testing/developing
 		return dataset;
+		
 	}
 
 	
@@ -61,38 +50,46 @@ public class FourierTransformImageOperation extends AbstractSimpleImageOperation
 	protected OperationData process(IDataset iDataset, IMonitor monitor) throws OperationException {
 		// Let's find out how big the image is datasetShape[height, width]
 		int[] datasetShape = iDataset.getShape();
-		datasetShape[0] -= 1;
-		datasetShape[1] -= 1;
-		Dataset dataset = (Dataset) iDataset;
-		Dataset transformedData = DatasetFactory.zeros(new int[]{datasetShape[0], datasetShape[1]}, dataset.getDType());
-//		SliceFromSeriesMetadata ssm = iDataset.getFirstMetadata(SliceFromSeriesMetadata.class);
-//		Slice[] slice = ssm.getSliceInOutput();
-//		SliceND plink = new SliceND(transformedData.getShape());
-//		plink.setSlice(1, 0, 200, 1);
+
+		IDataset floatDataset;
 		
+		//floatDataset = DatasetFactory.zeros(new int[]{datasetShape[0] + 1, datasetShape[1] + 1}, dataset.FLOAT64);
+
+		floatDataset = DatasetUtils.cast(FloatDataset.class, iDataset);
 		
+		// We need to typecast the int dataset to a float dataset, manually.
+//		for (int yIndex = 0; yIndex < datasetShape[0]; yIndex++) {
+//			for (int xIndex = 0; xIndex < datasetShape[0]; xIndex++) {
+//				floatDataset.set(dataset.getObject(yIndex, xIndex), yIndex, xIndex);
+//			}
+//		}
+
+		// Somewhere to put the transformed data
+		Dataset transformedData;
+		// The FFT itself!
+		transformedData = FFT.fft2(floatDataset, null, null);
 		
-		double valueToAssign = 0.00;
-		int currentIndex = 0;
-		
-		for (int yWave = 0; yWave < 50; yWave++) {
-			for (int xWave = 0; xWave < 50; xWave++) {
-				for (int ySpace = 0; ySpace < datasetShape[0]; ySpace++) {
-					for (int xSpace = 0; xSpace < datasetShape[1]; xSpace++) {
-						currentIndex = ySpace * xSpace;
-						valueToAssign = transformedData.getDouble(ySpace, xSpace);
-						valueToAssign += (dataset.getDouble(ySpace, xSpace) * Math.cos(2.0 * Math.PI * ((1.0 * xWave * xSpace / datasetShape[1]) + (1.0 * yWave * ySpace / datasetShape[0])))) / Math.sqrt(datasetShape[0] * datasetShape[1]);
-						transformedData.set(valueToAssign, ySpace, xSpace);
-						valueToAssign = 0.00;
-					}
-				}
-				System.out.println(xWave);
-			}
-		}
-		
+//		double valueToAssign = 0.00;
+//		int currentIndex = 0;
+//		
+////		for (int yWave = 0; yWave < 50; yWave++) {
+//	//		for (int xWave = 0; xWave < 50; xWave++) {
+//				for (int ySpace = 0; ySpace < datasetShape[0]; ySpace++) {
+//					for (int xSpace = 0; xSpace < datasetShape[1]; xSpace++) {
+//						currentIndex = ySpace * xSpace;
+//						valueToAssign = transformedData.getDouble(ySpace, xSpace);
+//						valueToAssign += (dataset.getDouble(ySpace, xSpace) * Math.cos(2.0 * Math.PI * ((1.0 * xWave * xSpace / datasetShape[1]) + (1.0 * yWave * ySpace / datasetShape[0])))) / Math.sqrt(datasetShape[0] * datasetShape[1]);
+//						transformedData.set(valueToAssign, ySpace, xSpace);
+//						valueToAssign = 0.00;
+//					}
+//				}
+//
+//		//	}
+//		//}
+//		
 		// So that DAWN doesn't crash whilst testing/developing
 		//OperationData toReturn = (OperationData) dataset;
-		OperationData toReturn = (OperationData) transformedData;
+		OperationData toReturn = new OperationData(transformedData);
 		
 		return toReturn;
 	}
