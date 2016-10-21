e4d43f7c7703aa52de010cf5c05f848b358702e7
Tim Snow <tim.snow@diamond.ac.uk>
2016-10-21 15:43:48 +0100

    2D FFT finished

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/image/FourierTransformImageOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/image/FourierTransformImageOperation.java
@@ -1,15 +1,27 @@
+/*-
+ * Copyright (c) 2016 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package uk.ac.diamond.scisoft.analysis.processing.operations.image;
 
-import org.eclipse.dawnsci.analysis.api.processing.OperationData;
-import org.eclipse.dawnsci.analysis.api.processing.OperationException;
-import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+// Imports from org.eclipse
 import org.eclipse.january.IMonitor;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.IDataset;
-import org.eclipse.dawnsci.analysis.dataset.impl.FFT;
 import org.eclipse.january.dataset.FloatDataset;
 import org.eclipse.january.dataset.DatasetUtils;
+import org.eclipse.dawnsci.analysis.dataset.impl.FFT;
+import org.eclipse.dawnsci.analysis.api.processing.OperationData;
+import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 
+
+//The operation for a DAWN process to perform a 2D FFT on images
 public class FourierTransformImageOperation extends AbstractSimpleImageOperation<FourierTransformImageModel> {
 
 	
