1dd872bc70460247ee6baf77e6d809dde4cc7338
Tim Snow <tim.snow@diamond.ac.uk>
2016-10-21 15:26:20 +0100

    Before sub-methoding

--- uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/image/FourierTransformImageOperation.java
+++ uk.ac.diamond.scisoft.analysis.processing/src/uk/ac/diamond/scisoft/analysis/processing/operations/image/FourierTransformImageOperation.java
@@ -37,10 +37,23 @@ public class FourierTransformImageOperation extends AbstractSimpleImageOperation
 		
 	// Now let's define the processImage method
 	@Override
-	public IDataset processImage(IDataset dataset, IMonitor monitor) throws OperationException {
+	public IDataset processImage(IDataset iDataset, IMonitor monitor) throws OperationException {
 
-		// So that DAWN doesn't crash whilst testing/developing
-		return dataset;
+		// First we need to create a placeholder for typecasting the data as the FFT algorithm can't take integers (needs floats)
+		Dataset floatDataset;
+		// Now, let's typecast the data
+		floatDataset = DatasetUtils.cast(FloatDataset.class, iDataset);
+		
+		// Now we need a container for the transformed data
+		IDataset transformedData;
+		// Now we can do the FFT itself
+		transformedData = FFT.fft2(floatDataset, null, null);
+
+		// Now we can make the dataset, ready to return it to DAWN
+		OperationData toReturn = new OperationData(transformedData);
+		
+		// And then return it!
+		return toReturn;
 		
 	}
 
@@ -48,49 +61,21 @@ public class FourierTransformImageOperation extends AbstractSimpleImageOperation
 	// Now let's define the process method
 	@Override
 	protected OperationData process(IDataset iDataset, IMonitor monitor) throws OperationException {
-		// Let's find out how big the image is datasetShape[height, width]
-		int[] datasetShape = iDataset.getShape();
 
-		IDataset floatDataset;
-		
-		//floatDataset = DatasetFactory.zeros(new int[]{datasetShape[0] + 1, datasetShape[1] + 1}, dataset.FLOAT64);
-
+		// First we need to create a placeholder for typecasting the data as the FFT algorithm can't take integers (needs floats)
+		Dataset floatDataset;
+		// Now, let's typecast the data
 		floatDataset = DatasetUtils.cast(FloatDataset.class, iDataset);
 		
-		// We need to typecast the int dataset to a float dataset, manually.
-//		for (int yIndex = 0; yIndex < datasetShape[0]; yIndex++) {
-//			for (int xIndex = 0; xIndex < datasetShape[0]; xIndex++) {
-//				floatDataset.set(dataset.getObject(yIndex, xIndex), yIndex, xIndex);
-//			}
-//		}
-
-		// Somewhere to put the transformed data
-		Dataset transformedData;
-		// The FFT itself!
+		// Now we need a container for the transformed data
+		IDataset transformedData;
+		// Now we can do the FFT itself
 		transformedData = FFT.fft2(floatDataset, null, null);
-		
-//		double valueToAssign = 0.00;
-//		int currentIndex = 0;
-//		
-////		for (int yWave = 0; yWave < 50; yWave++) {
-//	//		for (int xWave = 0; xWave < 50; xWave++) {
-//				for (int ySpace = 0; ySpace < datasetShape[0]; ySpace++) {
-//					for (int xSpace = 0; xSpace < datasetShape[1]; xSpace++) {
-//						currentIndex = ySpace * xSpace;
-//						valueToAssign = transformedData.getDouble(ySpace, xSpace);
-//						valueToAssign += (dataset.getDouble(ySpace, xSpace) * Math.cos(2.0 * Math.PI * ((1.0 * xWave * xSpace / datasetShape[1]) + (1.0 * yWave * ySpace / datasetShape[0])))) / Math.sqrt(datasetShape[0] * datasetShape[1]);
-//						transformedData.set(valueToAssign, ySpace, xSpace);
-//						valueToAssign = 0.00;
-//					}
-//				}
-//
-//		//	}
-//		//}
-//		
-		// So that DAWN doesn't crash whilst testing/developing
-		//OperationData toReturn = (OperationData) dataset;
+
+		// Now we can make the dataset, ready to return it to DAWN
 		OperationData toReturn = new OperationData(transformedData);
 		
+		// And then return it!
 		return toReturn;
 	}
 	
