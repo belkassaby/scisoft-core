402a0e8e3aafd4b66f1115c1a083e36349d4aa02
Dean <dean.ottewell@diamond.ac.uk>
2017-02-07 12:23:19 +0000

    Added base implementation for managing data with params across indexers

---
+++ uk.ac.diamond.scisoft.analysis.powder.indexer/src/uk/ac/diamond/scisoft/analysis/powder/diffraction/indexer/AbstractPowderIndexerData.java
@@ -0,0 +1,70 @@
+package uk.ac.diamond.scisoft.analysis.powder.diffraction.indexer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.eclipse.dawnsci.analysis.api.peakfinding.IPeakFinderParameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * TODO: in many ways this is abstract containing basic essientials of a powderindexerdata
+ * ...wavelength only essentials
+ * 
+ * TODO: order of list matters...
+ * 
+ * @author Dean P. Ottewell
+ *
+ */
+public abstract class AbstractPowderIndexerData implements IPowderIndexerData {
+
+	/* Add logging facility */
+	protected static transient final Logger logger = LoggerFactory.getLogger(AbstractPowderIndexerData.class);
+	
+	protected String indexerName;
+	private double wavelength;
+	private List<IPowderIndexerParam> indexerParams = new ArrayList<IPowderIndexerParam>();
+	
+	
+	protected AbstractPowderIndexerData() {
+		//TODO: wait to pass themselves...
+		indexerParams.add(new PowderIndexerParam("Wavelength", this.wavelength));
+	}
+	
+	protected abstract void setName();
+
+	protected abstract String getFormatedIndexerParam(IPowderIndexerParam param);
+	
+	protected void setWavelength(double wavelength){
+		this.wavelength = wavelength;
+	}
+	
+	protected double getWavelength(){
+		return this.wavelength;
+	}
+	
+	@Override
+	public String getIndexerName() {
+		return this.indexerName;
+	}
+
+	@Override
+	public List<IPowderIndexerParam> getParameters() {
+		return indexerParams;
+	}
+
+	@Override
+	public IPowderIndexerParam getParameter(String pName) throws Exception {
+		//TODO: find param... maybe
+		return null;
+	}
+
+	@Override
+	public void setParameter(IPowderIndexerParam param) throws Exception {
+		indexerParams.add(param);
+	}
+
+}
---
+++ uk.ac.diamond.scisoft.analysis.powder.indexer/src/uk/ac/diamond/scisoft/analysis/powder/diffraction/indexer/NtreorPowderIndexerData.java
@@ -0,0 +1,61 @@
+package uk.ac.diamond.scisoft.analysis.powder.diffraction.indexer;
+
+import org.eclipse.january.dataset.IDataset;
+
+public class NtreorPowderIndexerData extends AbstractPowderIndexerData {
+
+	@Override
+	protected void setName() {
+		this.indexerName = "ntreor";
+	}
+
+	@Override
+	protected String getFormatedIndexerParam(IPowderIndexerParam param) {
+		return param.getName() + "=" + param.getValue() + ",";
+	}
+	
+	public String formatPeakData(IDataset peaks){
+		//TODO: validate
+		String peaksFormatted = null;
+		
+		for (int i = 0; i < peaks.getSize(); ++i) {
+			double d = peaks.getDouble(i);
+			peaksFormatted += String.valueOf(d) + "\n";
+		}
+		
+		return peaksFormatted;
+	}
+	
+	//The old func
+	//	public void generateIndexFile(String fullPathFile) {
+//		try {
+//			PrintWriter writer = new PrintWriter(fullPathFile, "UTF-8");
+//
+//			writer.println(outFileTitle);
+//
+//			// write in the peak data
+//			for (int i = 0; i < peakData.getSize(); ++i) {
+//				double d = peakData.getDouble(i);
+//				writer.println(String.valueOf(d));
+//			}
+//
+//			writer.println();
+//
+//			for (Entry<String, String> entry : stdKeyVals.entrySet()) {
+//				String key = entry.getKey();
+//				Object value = entry.getValue();
+//				writer.println(key + "=" + value + ",");
+//			}
+//
+//			// finish file
+//			writer.println("END*");
+//			writer.println("0.00");
+//			writer.close();
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+
+}
