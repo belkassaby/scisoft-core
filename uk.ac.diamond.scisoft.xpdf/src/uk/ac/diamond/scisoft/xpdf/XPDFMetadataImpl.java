e6ca44b376125390396120b80ea2d7d5890f5485
Tim Spain <timothy.spain@diamond.ac.uk>
2017-10-30 14:17:58 +0000

    First pass at XRMC processing for XPDF.

--- uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/XPDFMetadataImpl.java
+++ uk.ac.diamond.scisoft.xpdf/src/uk/ac/diamond/scisoft/xpdf/XPDFMetadataImpl.java
@@ -49,6 +49,7 @@ public class XPDFMetadataImpl implements XPDFMetadata {
 	private double fluoro;
 	private double lorchCutOff;
 	private double comptonScaling = 1.0;
+	private List<Dataset> incoherentScatteringData;
 	
 	/**
 	 * Empty constructor.
@@ -62,6 +63,7 @@ public class XPDFMetadataImpl implements XPDFMetadata {
 		emptyDataParameters = null;
 		absorptionCorrectionMaps = null;
 		tect = null;
+		incoherentScatteringData = new ArrayList<Dataset>();
 	}
 	
 	/**
@@ -95,6 +97,12 @@ public class XPDFMetadataImpl implements XPDFMetadata {
 		this.calCon = inMeta.calCon;
 		this.fluoro = inMeta.fluoro;
 		this.lorchCutOff = inMeta.lorchCutOff;
+		
+		// Deep copy of the incoherent scattering data
+		this.incoherentScatteringData = new ArrayList<Dataset>();
+		for (Dataset incohere: inMeta.incoherentScatteringData) {
+			this.pushIncoherentScattering(incohere.clone());
+		}
 	}
 	
 	/**
@@ -515,6 +523,16 @@ public class XPDFMetadataImpl implements XPDFMetadata {
 	@Override
 	public double getComptonScaling() {
 		return this.comptonScaling;
+	}
+
+	@Override
+	public void pushIncoherentScattering(Dataset data) {
+		this.incoherentScatteringData.add(data);
+	}
+	
+	@Override
+	public Dataset getIncoherentScattering(int index) {
+		return this.incoherentScatteringData.get(index);
 	}
 	
 }
