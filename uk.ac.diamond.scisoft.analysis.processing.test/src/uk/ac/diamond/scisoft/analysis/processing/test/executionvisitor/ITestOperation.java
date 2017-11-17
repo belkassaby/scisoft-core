fa4c90f3500b6c1c67ce431af96181beac792b3c
Baha El Kassaby <baha.elkassaby@gmail.com>
2017-11-17 19:08:27 +0100

    Move hdf.object tests in processing to ncd.processing

--- uk.ac.diamond.scisoft.analysis.processing.test/src/uk/ac/diamond/scisoft/analysis/processing/test/executionvisitor/ITestOperation.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.diamond.scisoft.analysis.processing.test.executionvisitor;
-
-import org.eclipse.dawnsci.analysis.api.processing.OperationData;
-
-public interface ITestOperation {
-
-	public OperationData getTestData();
-}
